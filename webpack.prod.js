dcb0a029d50da43c4fb3624593d627aa3d1e17fe
suleiman <sl.mustapha98@gmail.com>
2020-09-30 15:40:31 +0100

    add utility 'webpack-merge'

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,10 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  entry: "./src/client/index.js",
+module.exports = merge(common, {
   mode: "production",
   optimization: {
     minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
@@ -14,21 +12,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: "/.js$/",
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/client/views/index.html",
-      filename: "./index.html",
-    }),
-    new MiniCssExtractPlugin({ filename: "[name].css" }),
-  ],
-};
+  plugins: [new MiniCssExtractPlugin({ filename: "[name].css" })],
+});
